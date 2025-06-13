75b853a59e1c1158bf36ea8846be33a87378e112
Cooper Black's <cooperblackdboss@gmail.com>
2025-06-13 22:18:49 +0600

    Update emojiHandling.js

--- asset/js/emojiHandling.js
+++ asset/js/emojiHandling.js
@@ -1,22 +1,4 @@
 // /asset/js/emojiHandling.js
 import { emojiBtn, emojiPicker, messageInput } from "./globals.js";
 
-emojiBtn.addEventListener("click", () => {
-  emojiPicker.style.display =
-    emojiPicker.style.display === "block" ? "none" : "block";
-  if (emojiPicker.style.display === "block") {
-    emojiPicker.innerHTML = `
-      <span class="cursor-pointer" onclick="addEmoji('😊')">😊</span>
-      <span class="cursor-pointer" onclick="addEmoji('👍')">👍</span>
-      <span class="cursor-pointer" onclick="addEmoji('❤️')">❤️</span>
-      <span class="cursor-pointer" onclick="addEmoji('😂')">😂</span>
-    `;
-  }
-});
-
-window.addEmoji = function (emoji) {
-  messageInput.value += emoji;
-  emojiPicker.style.display = "none";
-};
-
-// Previous code had an 'emoji-click' event
+// no need of this currently
